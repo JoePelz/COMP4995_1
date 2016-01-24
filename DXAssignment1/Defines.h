7b74d71381c6f5bc96594c7b064a32c05377288a
Joe Pelz <joe.pelz@gmail.com>
2016-01-23 17:12:41 -0800

    removed unused macro

--- DXAssignment1/Defines.h
+++ DXAssignment1/Defines.h
@@ -3,7 +3,6 @@
 #define DEFAULT_BITMAP "fields_medium.bmp"
 #define DEFAULT_WIDTH 640
 #define DEFAULT_HEIGHT 480
-#define MAP(x, y) (y * LockedRect.Pitch / 4 + x)
 #define CLEAR_COLOR D3DCOLOR_XRGB(0, 0, 25)
 #define FRAME_RATE_SMOOTHING 50
 
